b2acc66e37884b724339332662303350e97d12c2
wuxingyuyouxing <wuxingyuyouxing@outlook.com>
2026-08-19 10:54:47 +0800

    chore: 将误提交的 plugins/.bin 移出版本控制（工作区文件保留，已加入 .gitignore）

--- plugins/dsh-deep-whale-main/orca-link/tsdown.config.ts
+++
@@ -1,5 +0,0 @@
-import { clientBundle } from './build/tsdown.client.ts'
-
-export default clientBundle('@dsh-external/dsh-client-ui-skin-orca-link', ['src/index.ts'], {
-  portableCssModuleIds: true,
-})
